435a278e2db0945d99ab9e35994f2f272041c055
ZachMommaerts <zmommaerts@gmail.com>
2023-03-02 19:19:23 -0600

    Initial Commit

---
+++ Swift_Slots/Swift_SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_SlotsApp.swift
+//  Swift_Slots
+//
+//  Created by Zach Mommaerts on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
